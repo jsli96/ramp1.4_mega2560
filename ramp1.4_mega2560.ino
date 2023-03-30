8e5aadd9872fef91124d151ba4e3714b876d492e
jiashengli <jsli@umd.edu>
2023-03-30 18:05:41 -0400

    sdfdsdf

--- ramp1.4_mega2560.ino
+++ ramp1.4_mega2560.ino
@@ -8,25 +8,26 @@
 #define MEASURE_Y_DIR 34
 #define Y_STOP_PIN 14
 
-
-AccelStepper myStepper(AccelStepper::DRIVER, MEASURE_X_STEP, MEASURE_X_DIR);
+AccelStepper stepper_measure_x(AccelStepper::DRIVER, MEASURE_X_STEP, MEASURE_X_DIR);
+AccelStepper stepper_measure_y(AccelStepper::DRIVER, MEASURE_Y_STEP, MEASURE_Y_DIR);
 String inString = ""; // string to hold input
 int targetPos;
-
 
 void setup()
 {
   Serial.begin(9600);
-  myStepper.enableOutputs();
-  myStepper.setMaxSpeed(20000);
-  myStepper.setSpeed(20000);
-  myStepper.setAcceleration(20000);
+  stepper_measure_x.setMaxSpeed(20000);
+  stepper_measure_x.setSpeed(20000);
+  stepper_measure_x.setAcceleration(20000);
+  stepper_measure_y.setMaxSpeed(20000);
+  stepper_measure_y.setSpeed(20000);
+  stepper_measure_y.setAcceleration(20000);
   pinMode(MEASURE_X_EN, OUTPUT);
   digitalWrite(MEASURE_X_EN, HIGH);
   pinMode(MEASURE_Y_EN, OUTPUT);
   digitalWrite(MEASURE_Y_EN, HIGH);
-  pinMode(X_STOP_PIN,INPUT);
-  pinMode(Y_STOP_PIN,INPUT);
+  pinMode(X_STOP_PIN, INPUT);
+  pinMode(Y_STOP_PIN, INPUT);
   initial_calibration();
 }
 
@@ -49,26 +50,43 @@ void loop()
   //     // clear the string for new input:
   //     inString = "";
   //   }
-  //   myStepper.moveTo(targetPos);
-  //   while (myStepper.currentPosition() != targetPos)
+  //   stepper_measure_x.moveTo(targetPos);
+  //   while (stepper_measure_x.currentPosition() != targetPos)
   //   {
-  //     myStepper.run();
+  //     stepper_measure_x.run();
   //   }
   // }
 }
 
-
-void initial_calibration(){
+void initial_calibration()
+{
+  Serial.print("Start calibration process.\n");
   int home_x = digitalRead(X_STOP_PIN);
   int home_y = digitalRead(Y_STOP_PIN);
-  while (home_x != 0 ){
-    myStepper.move(1);
-    myStepper.run();
+  while (home_x != 0)
+  {
+    stepper_measure_x.move(10);
+    stepper_measure_x.run();
     home_x = digitalRead(X_STOP_PIN);
+    if (home_x == 0)
+    {
+      Serial.println("Measuring device arrive X-axis home position.\n");
+      stepper_measure_x.setCurrentPosition(0);
+      Serial.println("Measuring device X-axis position has been reset to zero.\n");
+    }
   }
-  if (home_x == 0){
-    Serial.println("Measuring device arrive X-axis home position.");
-    myStepper.setCurrentPosition(0);
-    Serial.println("Measuring device X-axis position has been reset to zero.");
+
+  while (home_y != 0)
+  {
+    stepper_measure_y.move(10);
+    stepper_measure_y.run();
+    home_y = digitalRead(Y_STOP_PIN);
+    if (home_y == 0)
+    {
+      Serial.println("Measuring device arrive Y-axis home position.\n");
+      stepper_measure_y.setCurrentPosition(0);
+      Serial.println("Measuring device Y-axis position has been reset to zero.\n");
+    }
   }
+  Serial.println("Initial Calibration complete!\n");
 }
