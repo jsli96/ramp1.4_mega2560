a303b976d8e0eb72272d60f79d89415f3d9b47d3
jiashengli <jsli@umd.edu>
2023-07-03 20:32:52 -0400

    add active haptic feedback device code

--- haptic-device_mega2560/haptic-device_mega2560.ino
+++ haptic-device_mega2560/haptic-device_mega2560.ino
@@ -1,19 +1,27 @@
-# define y_Enable A8
+#include <AccelStepper.h>
+#include <elapsedMillis.h>
+# define y_Enable A2
 # define y_Step A6
 # define y_Dir A7
-# define x_Enable A2
+# define x_Enable 38
 # define x_Step A0
 # define x_Dir A1
+
+AccelStepper myStepper_y(AccelStepper::DRIVER, y_Step, y_Dir, true); 
+AccelStepper myStepper_x(AccelStepper::DRIVER, x_Step, x_Dir, true); 
+elapsedMillis printTime;
 
 
 void setup(){
     Serial.begin(9600);
-    pinMode(y_Dir, OUTPUT);
+    myStepper_x.setMaxSpeed(1000.0);   // the motor accelerates to this speed exactly without overshoot. Try other values.
+    myStepper_x.setSpeed(200);
     pinMode(y_Enable, OUTPUT);
-    pinMode(y_Step, OUTPUT);
+    digitalWrite(y_Enable, HIGH);
+    pinMode(x_Enable, OUTPUT);
+    digitalWrite(x_Enable, HIGH);
 }
 
-void loop(){
-
-
+void loop() {
+  myStepper_x.runSpeed();
 }
